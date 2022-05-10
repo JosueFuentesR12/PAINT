6a7a3e75dbe69863b2cf0f3c4d997c856b799b43
JosueFuentesR12 <105233426+JosueFuentesR12@users.noreply.github.com>
2022-05-10 10:58:26 -0500

    Anotaciones
    FreeGames

--- paint.py
+++ paint.py
@@ -1,14 +1,9 @@
-"""Paint, for drawing shapes.
-
-Exercises
-
-1. Add a color.
-2. Complete circle.
-3. Complete rectangle.
-4. Complete triangle.
-5. Add width parameter.
 """
-
+Juego: Paint
+Programador 1: Josué Fuentes
+Programador 2: Alex Flores
+Fecha: 10 / mayo / 2022
+"""
 
 from turtle import *
 
@@ -42,12 +37,14 @@ def circle(start, end):
     up()
     goto(start.x, start.y)
     down()
-    goto(end.x, end.y)
     begin_fill()
 
-    for count in range(30):
-        forward(end.x - start.x)
-        left(12)
+    #Partimos de los 360 grados que tiene un circulo y lo dividimos entre 10 para hacer una equivalencia
+    for count in range(36):
+        #Se limita la distancia de la linea
+        forward(10)
+        #De los 100 al dividirlo, lo marcamos a 10 para la equivalencia
+        left(10)
 
 
 def rectangle(start, end):
@@ -57,8 +54,13 @@ def rectangle(start, end):
     down()  
     begin_fill()
 
-    for count in range(4):
-        forward(start.x - end.y)
+    #Se limitan en total 4 lados, divididos en 2 por el eje x y el y
+    for count in range(2):
+        #Se marca el lado x del rectángulo
+        forward(start.x)
+        left(90)
+        #Se marca el lado y del rectángulo
+        forward(end.y)
         left(90)
 
     end_fill()
@@ -71,8 +73,10 @@ def triangle(start, end):
     down()
     begin_fill()
 
+    #Se limita un rango de 3 grios (los lados)
     for count in range(3):
         forward(end.x - start.x)
+        #Su ángulo de giro sera de 120 para formar tríangulo equilatero
         left(120)
 
     end_fill()
@@ -106,6 +110,7 @@ onkey(lambda: color('white'), 'W')
 onkey(lambda: color('green'), 'G')
 onkey(lambda: color('blue'), 'B')
 onkey(lambda: color('red'), 'R')
+#Se agrega el color naranja
 onkey(lambda: color('orange'), 'O')
 onkey(lambda: store('shape', line), 'l')
 onkey(lambda: store('shape', square), 's')
