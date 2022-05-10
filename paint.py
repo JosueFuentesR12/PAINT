a22f0a54b230e5db92fb77f9392dabd40559e400
JosueFuentesR12 <105233426+JosueFuentesR12@users.noreply.github.com>
2022-05-10 10:53:55 -0500

    Se agregaron anotaciones y la solución del circulo
    Python FreeGames Edit

--- paint.py
+++ paint.py
@@ -1,12 +1,8 @@
-"""Paint, for drawing shapes.
-
-Exercises
-
-1. Add a color.
-2. Complete circle.
-3. Complete rectangle.
-4. Complete triangle.
-5. Add width parameter.
+"""
+Juego: Paint
+Programador 1: Josué Fuentes
+Programador 2: ??????
+Fecha: 9 / mayo / 2022
 """
 
 from turtle import *
@@ -38,17 +34,52 @@ def square(start, end):
 
 def circle(start, end):
     """Draw circle from start to end."""
-    pass  # TODO
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
+
+    #Partimos de los 360 grados que tiene un circulo y lo dividimos entre 10 para hacer una equivalencia
+    for count in range(36):
+        #Se limita la distancia de la linea
+        forward(10)
+        #De los 100 al dividirlo, lo marcamos a 10 para la equivalencia
+        left(10)
 
 
 def rectangle(start, end):
-    """Draw rectangle from start to end."""
-    pass  # TODO
+    """Draw square from start to end."""
+    up()
+    goto(start.x, start.y)
+    down()  
+    begin_fill()
+
+    #Se limitan en total 4 lados, divididos en 2 por el eje x y el y
+    for count in range(2):
+        #Se marca el lado x del rectángulo
+        forward(start.x)
+        left(90)
+        #Se marca el lado y del rectángulo
+        forward(end.y)
+        left(90)
+
+    end_fill()
 
 
 def triangle(start, end):
     """Draw triangle from start to end."""
-    pass  # TODO
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
+
+    #Se limita un rango de 3 grios (los lados)
+    for count in range(3):
+        forward(end.x - start.x)
+        #Su ángulo de giro sera de 120 para formar tríangulo equilatero
+        left(120)
+
+    end_fill()
 
 
 def tap(x, y):
@@ -79,6 +110,8 @@ onkey(lambda: color('white'), 'W')
 onkey(lambda: color('green'), 'G')
 onkey(lambda: color('blue'), 'B')
 onkey(lambda: color('red'), 'R')
+#Se agrega el color naranja
+onkey(lambda: color('orange'), 'O')
 onkey(lambda: store('shape', line), 'l')
 onkey(lambda: store('shape', square), 's')
 onkey(lambda: store('shape', circle), 'c')
